d470bbbe36da6581677d405a5f8147a0c017882a
mallvinorz <mallvino1216@gmail.com>
2022-10-15 12:56:04 +0700

    modify main menu scene

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -10,6 +10,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
     }
+
+    public void Credit()
+    {
+        Debug.Log("Credit ...");
+    }
     public void ExitGame()
     {
         Debug.Log("Quit !!!");
